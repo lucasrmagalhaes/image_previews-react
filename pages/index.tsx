2655cefefe5a89c1e30beed68c52bf0b2ca8c68a
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-07-09 15:02:24 -0300

    Previewing Image with FileReader

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,78 @@
-import { useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export default function Home() {
+  const [image, setImage] = useState<File>()
+  const [preview, setPreview] = useState<string>()
   const fileInputRef = useRef<HTMLInputElement>()
 
+  useEffect(() => {
+    if (image) {
+      const reader = new FileReader()
+      reader.onloadend = () => {
+        setPreview(reader.result as string)
+      }
+      reader.readAsDataURL(image)
+    } else {
+      setPreview(null)
+    }
+  }, [image])
+
   return (
-    <div>
-      <form 
-        style={{ 
-          display: "flex",
-          justifyContent: "center",
-        }}>
-        <button 
-          style={{ 
-            width: "200px",
-            height: "200px",
-            borderRadius: "50%",
-            cursor: "pointer",
-          }}
-          onClick={(event) => {
-            event.preventDefault()
-            fileInputRef.current.click()
-          }}
-        >
+    <div         
+      style={{
+        minHeight: "100vh",
+        padding: "0 0.5rem",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <form>
+        {preview ? (
+          <img 
+            src={preview} 
+            style={{ 
+              objectFit: "cover",
+              width: "200px",
+              height: "200px",
+              borderRadius: "50%",
+              cursor: "pointer", 
+            }} 
+          />
+        ) : (
+          <button 
+            style={{ 
+              width: "200px",
+              height: "200px",
+              borderRadius: "50%",
+              cursor: "pointer",
+            }}
+            onClick={(event) => {
+              event.preventDefault()
+              fileInputRef.current.click()
+            }}
+          >
             Adicionar Imagem
           </button>
-        <input type="file" style={{ display: "none" }} ref={fileInputRef} />
+        )}  
+        <input 
+          style={{ 
+            display: "none" 
+          }}
+          type="file"
+          accept="image/*"
+          ref={fileInputRef}
+          onChange={(event) => {
+            const file = event.target.files[0]
+
+            if (file && file.type.substr(0, 5) === "image") {
+              setImage(file)
+            } else {
+              setImage(null)
+            }
+          }} 
+        />
       </form>
     </div>
   )
